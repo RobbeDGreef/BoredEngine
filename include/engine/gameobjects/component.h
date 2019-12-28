1e157d02bca1ef5dba3066aaff5ec5e8efda31c1
RobbeDGreef <robbedegreef@gmail.com>
2019-12-29 00:19:18 +0100

    Removed unnecessary comment and added collisiontypes

--- include/engine/gameobjects/component.h
+++ include/engine/gameobjects/component.h
@@ -11,54 +11,56 @@
 #ifndef BE_COMPONENT_H
 #define BE_COMPONENT_H
 
+#include <engine/movesystem.h>
 #include <engine/transformable.h>
-#include <fw/event.h>
 #include <engine/eventdispatcher.h>
+#include <fw/event.h>
 #include <core.h>
 
 namespace be
 {
 
+class MoveSystem;
 class ComponentParent;
 
 class Component : public Transformable
 {
 
-/**
- * Hersenspinsels:
- * so if this is an actor he needs to handle that completely like a new one right ? but with transformable 
- * info of the parent? so that means that every actor somehow is a level too ?
- * ok so if that's true that means we could create a class that holds a list of components (including actors)
- * and inherit actor from this class AND component right? or nONONON better idea
- * 
- * let's make component also hold transformable info yeayeayea that's better and that way every actor holds it's own work postition etc
- * which makes so much more sense really
- * 
- * so to recap
- * actor -> inherits from component
- * component -> inherits from transformable class
- * component: renderable, code exec etc...
- */
+public:
+    enum ComponentType
+    {
+        Collision,
+        Renderable,
+        SceneRoot,
+        Actor
+    };
+
 private:
-    bool m_renderAble;
+    int m_type; // Type of component see enum ComponentType
     bool m_events;
     bool m_isParent;
+    ComponentParent *m_parent;
     
     bool m_sceneRoot;
-    ComponentParent *m_parent;
+    MoveSystem *m_moveSystem;
 
 public:
-    int gameState;
+   int gameState;
 
 public:
     // Engine functions 
-    Component(bool);
-    Component(ComponentParent *, bool render, bool event, bool parent);
+    Component(MoveSystem *movesys);
+    Component(ComponentParent *, int type, bool event, bool parent);
     virtual void onRender() {}
 
-    bool isRenderable() { return m_renderAble; }
+    int getType() { return m_type; }
+    void setType(int type) { m_type = type; }
     bool isParent() { return m_isParent; }
+    ComponentParent *getParent() { return m_parent; }
+    MoveSystem *getMoveParent(){ return m_moveSystem; }
+    bool isSceneRoot() { return m_sceneRoot; }
     bool getsEvents() { return m_events; }
+    
 
     // Functions called by user of engine 
 
@@ -88,7 +90,7 @@ public:
     void addChild(Component *, Vector2 <float> , Rotator <float> , Vector2 <float>);
     
     auto getChildren() { return m_children; }
-    ComponentParent(EventDispatcher *ed);
+    ComponentParent(MoveSystem *movesys, EventDispatcher *ed);
     ComponentParent(ComponentParent *parent, bool render, bool event);
 };
 
