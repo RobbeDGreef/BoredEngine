c283e449caeebdbfbf9578fe703f242684eb3dc7
RobbeDGreef <robbedegreef@gmail.com>
2019-12-29 15:44:38 +0100

    Added timer to layer to calc dt, removed movesys

--- include/engine/layer.h
+++ include/engine/layer.h
@@ -11,16 +11,20 @@
 #ifndef BE_LAYER_H
 #define BE_LAYER_H
 
-#include <engine/movesystem.h>
 #include <core.h>
 #include <fw/event.h>
 #include <fw/eventsystem.h>
+#include <fw/timer.h>
 
 namespace be
 {
 
 class Layer : public EventDispatcher
 {
+
+private:
+    Timer m_timer;
+
 public:
     ComponentParent *root;
     bool m_renderAble = false;
@@ -29,16 +33,24 @@ public:
 protected:
     std::string m_layerName;
 #endif
-    MoveSystem *m_moveSystem;
 
 public:
 
 #ifdef DEBUG_BUILD
-    Layer(std::string layername, bool isRenderAble, MoveSystem *);
+    Layer(std::string layername, bool isRenderAble);
 #endif
-    Layer(bool isRenderAble, MoveSystem *);
+    Layer(bool isRenderAble);
     ~Layer();
     
+    /// @brief this function will be called by layerstack and this function will update the timer and call the onUpdate function
+    void update();
+
+    /**
+     * @brief Get the elapsed time in milliseconds
+     * 
+     * @return unsigned int elapsed time in milliseconds
+     */
+    float getElapsedTime();
     virtual void onUpdate() {}
     virtual void onAttach() {}
     virtual void onDetach() {}
