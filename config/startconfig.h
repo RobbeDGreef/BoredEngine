e6c34ac7aceac822420bac714ce467cef93bbc76
RobbeDGreef <robbedegreef@gmail.com>
2019-12-30 19:53:37 +0100

    Default entitites now handeled by gamecontext

--- config/startconfig.h
+++ config/startconfig.h
@@ -11,8 +11,29 @@
 #ifndef BE_STARTCONFIG_H
 #define BE_STARTCONFIG_H
 
-// Uncomment this next two lines and reference your own startlevel in the same manner, the name of the time should still be STARTLEVEL
+#include <engine/gamecontext.h>
+
+#include <engine/gameobjects/playercontroller.h>
 #include <engine/gameobjects/level.h>
-using STARTLEVEL = be::Level;
+
+class GAMECONTEXT: public be::GameContext
+{
+public:
+    /**
+     * @brief Construct a new GAMECONTEXT object
+     * 
+     * @note I know code in header files is not good practice but I'm trying
+     *       to make it easier for the developer to configure their game.
+     */
+    GAMECONTEXT()
+    {
+        /**
+         * To change the startlevel, playercontroller etc import your own derived classes
+         * and change the classes after the 'new' specifier to your classes.
+         */
+        level = new Level(this);
+        playerController = new PlayerController(level->root);
+    }
+};
 
 #endif // BE_STARTCONFIG_H
