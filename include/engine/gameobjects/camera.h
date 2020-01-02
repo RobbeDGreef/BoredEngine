84d02b48623d20596e31acd330f3874d67a7f78f
RobbeDGreef <robbedegreef@gmail.com>
2020-01-02 18:54:51 +0100

    Gave camera zooming capabilities

--- include/engine/gameobjects/camera.h
+++ include/engine/gameobjects/camera.h
@@ -3,6 +3,8 @@
 
 #include <engine/gameobjects/component.h>
 
+#define DEFAULT_CAMERA_DISTANCE 20
+
 namespace be
 {
 
@@ -12,9 +14,14 @@ class Camera: public Component
 private:
     unsigned int m_width;
     unsigned int m_height;
+    float m_zoom;
 
 public:
     Camera(ComponentParent *parent, Vector2<float> location, unsigned int width, unsigned int height);
+    float getZoom();
+
+    void setZoom(float zoom);
+    void setDistance(unsigned int distance);
 };
 
 } // namespace be
--- src/engine/gameobjects/camera.cpp
+++ src/engine/gameobjects/camera.cpp
@@ -1,6 +1,6 @@
 #include <engine/gameobjects/camera.h>
 #include <engine/gameobjects/component.h>
-#include <core.h>
+#include <engine.h>
 
 using namespace be;
 
@@ -8,5 +8,21 @@ Camera::Camera(ComponentParent *parent, Vector2<float> location, unsigned int wi
 {
     m_width = width;
     m_height = height;
+    m_zoom = 1.0f;
 }
 
+float Camera::getZoom()
+{
+    return m_zoom;
+}
+
+void Camera::setZoom(float zoom)
+{
+    m_zoom = zoom;
+}
+
+void Camera::setDistance(unsigned int distance)
+{
+    // @todo: Isn't distance like a hyperbolic function? I think it isn't as simple as just dividing.
+    m_zoom = DEFAULT_CAMERA_DISTANCE / distance; 
+}
